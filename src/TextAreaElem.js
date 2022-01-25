6be58bbe8466250b6ec12da3a1b02e10961bdbec
MathieuLd <mlindenpro@gmail.com>
2022-01-25 23:57:26 +0100

    Adding addaptative text size when list become full

--- src/TextAreaElem.js
+++ src/TextAreaElem.js
@@ -19,11 +19,25 @@ class TextAreaElem extends React.Component {
         this.props.onDeleteList(this.props.index)
     }
 
-    render() { 
+    render() {
+      const lineNum = this.props.text.split("\n").length; 
+      const textAreaFontSize = (lineNum < 10) ? 'md' : (lineNum < 11 ? 'sm' : 'xs')
+     
         return (
-            <Box position='relative' w='20rem' minW='15rem' h='100%' >
-                <Textarea value={this.props.text} onChange={this.handelChangeList} resize={"none"} position="absolute" zIndex="0" h={"100%"}></Textarea>
-                {this.props.listNumber > 1 && <IconButton onClick={this.handleDeleteList} className="deleteButton" position="absolute" top="1px" right="1px" zIndex="1" p="0" icon={<BinIcon boxSize="2rem"/>}/>}
+            <Box position='relative' w='20rem' minW='15rem' h='100%' overflowY='auto' >
+                <Textarea value={this.props.text} fontSize={textAreaFontSize} onChange={this.handelChangeList} resize='none' position="absolute" zIndex="0" h={"100%"} 
+                  sx={{
+                    '&::-webkit-scrollbar': {
+                      width: '50px',
+                      borderRadius: '8px',
+                      backgroundColor: `rgba(0, 0, 0, 0.05)`,
+                    },
+                    '&::-webkit-scrollbar-thumb': {
+                      backgroundColor: `rgba(0, 0, 0, 0.05)`,
+                    },
+                  }}
+                ></Textarea>
+                {this.props.listNumber > 1 && <IconButton onClick={this.handleDeleteList} className="deleteButton" position="absolute" top="1px" right='1px' zIndex="1" p="0" icon={<BinIcon boxSize="2rem"/>}/>}
             </Box>
         );
     }
