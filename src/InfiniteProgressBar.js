3a680ddc09a931c127d072559462b2a8106525ad
MathieuLd <mlindenpro@gmail.com>
2022-02-06 18:28:20 +0100

    Adding styling to progressbar

--- src/InfiniteProgressBar.js
+++ src/InfiniteProgressBar.js
@@ -1,19 +1,18 @@
 import React from 'react'
+import { Flex, Box, useColorMode } from '@chakra-ui/react'
 
-class InfiniteProgressBar extends React.Component {
-    constructor(props) {
-        super(props);
-    }
 
-    render() { 
-        return (
-            <div>
-                <div className='progressbar'>
-                    <div className='inner' style={{animation: 'progress '+this.props.duration/1000+'s infinite linear'}}></div>
-                </div>
-            </div>
-        );
-    }
+function InfiniteProgressBar(props){
+
+  const {colorMode, toggleColorMode} = useColorMode();
+
+  return (
+    <Flex flexGrow='1' align='center' justify='center'>
+        <Box width='90%' minWidth='3em' height='0.8em' borderColor={colorMode === 'dark' ? 'whiteAlpha.300' : 'gray.200'} borderWidth='1px' borderStyle='solid' rounded='0.3em'>
+            <Box height='100%' bg={colorMode === 'dark' ? 'teal.600' : 'teal.200'} rounded='0.3em' style={{animation: 'progress '+props.duration/1000+'s infinite linear'}}></Box>
+        </Box>
+    </Flex>
+  );
 }
  
 export default InfiniteProgressBar;
